3502b95eb434116e00ccb76399cdc09acd37c166
Knight <karvin_jimenez@hotmail.com>
2024-06-19 21:34:49 -0400

    extra changes.

--- src/main.cpp
+++ src/main.cpp
@@ -114,17 +114,6 @@ void render() {
     SDL_RenderPresent(renderer);
 }
 
-const int FRAME_RATE = 60; 
-
-void capFrameRate(Uint32 frameStartTime) {
-
-    Uint32 frameTime = SDL_GetTicks() - frameStartTime;
-    
-    if (frameTime < 1000 / FRAME_RATE) {
-        SDL_Delay(1000 / FRAME_RATE - frameTime);
-    }
-}
-
 int main()
 {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER) < 0) {
@@ -165,8 +154,6 @@ int main()
         handleEvents();
         update(deltaTime);
         render();
-
-        capFrameRate(currentFrameTime);
     }
 
     quitGame();
